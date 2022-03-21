b0d67f861d2ca7d67c8c39fe14a570e72108837d
Samara Carvalho <samcarvalhos@hotmail.com>
2022-03-21 09:58:34 -0300

    setting mutations and creating user

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -24,7 +24,7 @@ export function Pagination({
         onPageChange,
         registersPerPage = 10
 }: PaginationProps) {
-    const lastPage = Math.floor(totalCountOfRegisters / registersPerPage)
+    const lastPage = Math.ceil(totalCountOfRegisters / registersPerPage)
 
     const previousPages = currentPage > 1 ? generatePageArray((currentPage - 1) - siblingsCount, currentPage - 1) :   []
 
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useRouter } from 'next/router'
+
 import {
     Box,
     Button,
@@ -10,14 +12,18 @@ import {
     VStack
 } from "@chakra-ui/react";
 
+import { useMutation } from 'react-query'
+
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from "../../components/Form/Input";
-
 import { Header } from "../../components/Header";   
 import { Sidebar } from "../../components/Sidebar";
+
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
     name: string;
@@ -36,14 +42,31 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+    const router = useRouter();
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post(`users`, {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['users'])
+        }
+    })
+
     const { register, handleSubmit, formState, formState: { errors }} = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 1500));
+        await createUser.mutateAsync(values)
 
-        console.log(values)
+        router.push('/users')
     }
 
     return (
